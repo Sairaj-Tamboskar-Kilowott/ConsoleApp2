3c4c2f866a19cfc465d47e47256e4aeb3832ade0
Sairaj-Tamboskar-Kilowott <92842383+Sairaj-Tamboskar-Kilowott@users.noreply.github.com>
2021-12-03 17:15:01 +0530

    commit

--- Program.cs
+++ Program.cs
@@ -1,8 +1,26 @@
-﻿
-int firstValue = 500;
-int secondValue = 600;
-int largerValue, smallervalue;
-largerValue = Math.Max(firstValue, secondValue);
-smallervalue = Math.Min(firstValue, secondValue);
-Console.WriteLine(largerValue);
-Console.WriteLine(smallervalue);
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+
+        static int[] array = { 40, 38, 37, 36, 34, 33, 32, 31, 30, 30, 30, 29, 26, 26, 19 };
+        public static void Main(string[] args)
+        {
+            var sp = new Stats();
+            sp.Numbers = array;
+
+            var x = sp.Mean();
+            Console.WriteLine($"Mean is {x}");
+            var y = sp.Median();
+            Console.WriteLine("Median is " + y);
+            var z = sp.Mode();
+            Console.WriteLine("Mode is " + z);
+        }
+    }
+}
